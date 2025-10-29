e6b572212ce05cbdd2d445626fbfb138bd0ee395
vanessa-cl <vanessalima951@gmail.com>
2025-10-29 19:45:36 -0300

    fix: ajuste no método buscar categorias

--- src/controllers/categoriaController.js
+++ src/controllers/categoriaController.js
@@ -1,74 +1,22 @@
-const CategoriaModel = require('../models/Categoria');
+const CategoriaModel = require("../models/Categoria");
 class Categoria {
-    buscar() {
-        return CategoriaModel.listarCategorias();
+  async buscar(req, res) {
+    try {
+      const categorias = await CategoriaModel.listarCategorias();
+      res.json(categorias);
+    } catch (error) {
+      res.status(500).json({ error: "Erro ao buscar categorias" });
     }
-    criar() {
-        return "Categoria criada com sucesso";
-    }
-    alterar(id) {
-        return "Categoria " + id + " atualizada com sucesso";
-    }
-    apagar(id) {
-        return "Categoria " + id + " deletada com sucesso";
-    }
-
-    /*
-    async init(pool) {
-        this.pool = pool;
-        await this.criarCategoria();
-    }
-
-    async criarCategoria() {
-        const sql = 
-        `CREATE TABLE categorias (
-            id INT AUTO_INCREMENT PRIMARY KEY,
-            nome VARCHAR(100) NOT NULL,
-            descricao TEXT,
-            entidadeReferenciada VARCHAR(100),
-            status VARCHAR(20) NOT NULL DEFAULT 'Ativa'
-        );`
-
-        try {
-            await this.pool.query(sql);
-            console.log('Tabela categorias criada com sucesso!');
-        } catch (error) {
-            console.error('Erro ao criar tabela categorias:', error);
-            
-            if (error.sqlMessage) console.log(error.sqlMessage);
-        }
-
-    }
-    */
+  }
+  criar() {
+    return "Categoria criada com sucesso";
+  }
+  alterar(id) {
+    return "Categoria " + id + " atualizada com sucesso";
+  }
+  apagar(id) {
+    return "Categoria " + id + " deletada com sucesso";
+  }
 }
 
 module.exports = new Categoria();
-
-//-----------------------------------------------
-
-/*
-const CategoriaModel = require('../models/Categoria');
-
-module.exports = {
-    async buscar(req, res) {
-        try {
-            const categorias = await CategoriaModel.listarCategorias();
-            res.json(categorias);
-        } catch (error) {
-            res.status(500).json({ error: 'Erro ao buscar categorias' });
-        }
-    },
-    criar(req, res) {
-        res.send('Categoria criada com sucesso');
-    },
-    alterar(req, res) {
-        const { id } = req.params;
-        res.send(`Categoria ${id} atualizada com sucesso`);
-    },
-    apagar(req, res) {
-        const { id } = req.params;
-        res.send(`Categoria ${id} deletada com sucesso`);
-    }
-};
-
-*/
