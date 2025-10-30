9115c474c55af3e4da010990346cb265062e5a7d
vanessa-cl <vanessalima951@gmail.com>
2025-10-30 19:34:35 -0300

    ajustes

--- src/controllers/healthController.js
+++
@@ -1,5 +0,0 @@
-module.exports = {
-  check: (req, res) => {
-    res.json({ status: 'OK', message: 'Servidor funcionando corretamente!' });
-  }
-};
--- src/routes/index.js
+++ src/routes/index.js
@@ -1,17 +1,11 @@
-const express = require('express');
-const router = express.Router();
-
 const routerCliente = require('./clienteRouter');
 const routerCategoria = require('./categoriaRouter');
-const healthController = require('../controllers/healthController');
 const routerQuestionario = require('./questionarioRouter');
 const routerPergunta = require('./perguntaRouter');
 
 module.exports = (app) => {
-  app.get('/api/health', healthController.check);
   app.use('/api/clientes', routerCliente);
   app.use('/api/categorias', routerCategoria);
   app.use('/api/perguntas', routerPergunta);
   app.use('/api/questionarios', routerQuestionario);
-  app.use('/api/perguntas', routerPergunta);
 };
