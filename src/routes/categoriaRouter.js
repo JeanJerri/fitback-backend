97bb51280e973f1b2a3d61f20f97efb10cec501d
vanessa-cl <vanessalima951@gmail.com>
2025-11-30 00:56:35 -0300

    fix: ajuste no router de categorias

--- src/routes/categoriaRouter.js
+++ src/routes/categoriaRouter.js
@@ -1,33 +1,24 @@
-const{Router} = require("express");
-
+const { Router } = require("express");
 const router = Router();
-
 const categoriaController = require("../controllers/categoriaController");
 
-router.get("/", async (req, res) => {
-    const listaCategorias = categoriaController.buscar();
-    listaCategorias.then(categorias => 
-        res.status(200).json(categorias)
-    ).catch(error => {
-        res.status(400).json({error: error.message});
-    })
+router.get("/", categoriaController.buscar);
+
+router.post("/", (req, res) => {
+  const resposta = categoriaController.criar();
+  res.send(resposta);
 });
 
-router.post('/', (req, res) => {
-    const resposta = categoriaController.criar();
-    res.send(resposta);
-})
+router.put("/:id", (req, res) => {
+  const { id } = req.params;
+  const resposta = categoriaController.alterar(id);
+  res.send(resposta);
+});
 
-router.put('/:id', (req, res) => {
-    const {id} = req.params; 
-    const resposta = categoriaController.alterar(id);
-    res.send(resposta);
-    })
-    
-router.delete('/:id', (req, res) => {
-    const {id} = req.params;
-    const resposta = categoriaController.apagar(id);
-    res.send(resposta);
-    })
+router.delete("/:id", (req, res) => {
+  const { id } = req.params;
+  const resposta = categoriaController.apagar(id);
+  res.send(resposta);
+});
 
-module.exports = router; 
+module.exports = router;
