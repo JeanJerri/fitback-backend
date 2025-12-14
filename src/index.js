773ef35076e78297e4d25ad4c888dc47f8afd3b7
vanessa-cl <vanessalima951@gmail.com>
2025-12-14 16:46:30 -0300

    removendo server.js

--- src/index.js
+++ src/index.js
@@ -1,21 +1,21 @@
 require("dotenv").config();
+
 const express = require("express");
 const morgan = require("morgan");
 const cors = require("cors");
 const helmet = require("helmet");
 
 const app = express();
+
 app.use(cors());
 app.use(helmet());
 app.use(morgan("dev"));
 app.use(express.json());
 
 require("./routes")(app);
+
 const PORT = process.env.PORT || 3001;
-app.listen(PORT, (erro) => {
-  if (erro) {
-    console.error("Erro ao iniciar o servidor:", erro);
-    return;
-  }
-  console.log(`Server running on http://localhost:${PORT}`);
+
+app.listen(PORT, () => {
+  console.log(`Server running on port ${PORT}`);
 });
